1575112e7ff53ed5c49d864309471c9407d6c99e
umby89 <umbertopumpo@gmail.com>
2022-02-14 22:02:42 +0100

    Primo commit

---
+++ MainClass.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//2136628
+//Server=localhost;Database=DotaProject;Trusted_Connection=True;
+using System;
+using Newtonsoft.Json;
+
+namespace DotaProject // Note: actual namespace depends on the project name.
+{
+    public class MainClass
+    {
+        HttpService _ApiCall = new HttpService();
+
+        public static object matchChat { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            var istanceOfMyClass = new MainClass();
+            Console.WriteLine("Bella");
+            var matchStats = JsonConvert.DeserializeObject<MatchDTO>(istanceOfMyClass.getDatas("2136628").Result);
+            matchChat = matchStats?.Chat;
+
+        }
+
+        public async Task<string> getDatas(string MatchId)
+        {
+            return await _ApiCall.GetData(MatchId).Result.Content.ReadAsStringAsync();
+        }
+
+    }
+}
